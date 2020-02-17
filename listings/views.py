ec1d57fcab56d997b546643f47be78d8f48249b7
Samrajya Thapa <samrajyathapa@hst000191856um.wifig1.olemiss.edu>
2020-02-17 00:02:35 -0600

    second

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
@@ -12,7 +12,20 @@ def index(request):
     return render(request, 'listings/listings.html',context )
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+
+    listing= get_object_or_404(Listing, pk=listing_id)
+
+    context={
+        'listing':listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    listings=Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator= Paginator(listings, 3)
+    page=request.GET.get('page')
+    paged_listings=paginator.get_page(page)
+
+    context={'listings':paged_listings}
+    return render(request, 'listings/search.html', context)
